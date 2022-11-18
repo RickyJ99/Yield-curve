322ffe2d7d2bccb9c072de76c689babaa94d7d08
RickyJ99 <riccardodalcero99@gmail.com>
2022-11-18 17:24:31 +0100

    fix date bugs

--- ex1.r
+++ ex1.r
@@ -1,4 +1,5 @@
 phi       <- 0.3
+
 prob      <- matrix(c(phi, 1 - phi), 2, 2)
 prob[, 2] <- c(1 - phi, phi)
 prob
@@ -6,24 +7,21 @@ sigma   <- 2
 beta    <- .96
 h       <- 1.05
 l       <- .97
-state   <- c(h, l)^-sigma
-
-
+state   <- c(h, l)
 yield_mcmc <- function(n, prob, state){
-    out <- c()
+    out <- list()
     for (count in 1:n) {
-        prob  <- prob   %*% prob
-        state <- state  %*% prob
-        y <- -(1 / count) * log(beta^count * (prob %*% state))
+        state <-  prob %*% state
+        y <- -(1 / count) * log(beta^count) +
+        log((state %*% c(0.3, .7)) ^-sigma)
         out <- cbind(out, y)
+        prob  <- prob   %*% prob
+        state <- state  %*% t(state)
     }
     return(out)
 }
-yield(1, prob, state)
-
-#.0468 h
+yield_mcmc(1, prob, state)
+yield_mcmc(2, prob, state)
+# .0468 h
 #0.658 l
-states <- state %*% t(state)^-sigma
-states
-y_log      <- -0.5 * log(beta^2 * ( %*% ))
-y_log
+
