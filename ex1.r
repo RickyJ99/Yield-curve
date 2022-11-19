c2a5042d8d460c82d71347cbf490f83c339bf8a5
RickyJ99 <riccardodalcero99@gmail.com>
2022-11-19 01:51:59 +0100

    final version

--- ex1.r
+++ ex1.r
@@ -1,19 +1,19 @@
+library(matlib, help, pos = 2, lib.loc = NULL)
 phi       <- 0.3
 
 prob      <- matrix(c(phi, 1 - phi), 2, 2)
 prob[, 2] <- c(1 - phi, phi)
 prob
-sigma   <- 2
+gamma  <- 2
 beta    <- .96
 h       <- 1.05
 l       <- .97
-state   <- c(h, l)
+state   <- c(h, l) ^ - gamma
 yield_mcmc <- function(n, prob, state){
     out <- list()
     for (count in 1:n) {
         state <-  prob %*% state
-        y <- -(1 / count) * log(beta^count) +
-        log((state %*% c(0.3, .7)) ^-sigma)
+        y <-  -(1 / count) * log(state) - log(beta)
         out <- cbind(out, y)
         prob  <- prob   %*% prob
         state <- state  %*% t(state)
@@ -22,6 +22,7 @@ yield_mcmc <- function(n, prob, state){
 }
 yield_mcmc(1, prob, state)
 yield_mcmc(2, prob, state)
+yield_mcmc(4, prob, state)
 # .0468 h
 #0.658 l
 
